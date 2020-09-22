1f237552aa826b27035b4bef011cc58e2cc83d32
raz <razavramov2@gmail.com>
2020-09-22 19:51:03 +0300

    6

---
+++ Desktop/portfolio-master/webpage/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+  
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html')
